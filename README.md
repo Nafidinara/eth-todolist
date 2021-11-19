faf4c1fe97d1fc4adefb17d14299925bae6d4f2e
Alfara Nafi Dinara <nafidinara@gmail.com>
2021-11-19 07:58:34 +0700

    initial

---
+++ README.md
@@ -0,0 +1,15 @@
+# Simple Todolist in ETH Blockchain
+This simple todolist that storing data in blockhain
+
+## Requirement
+1. Nodejs
+2. Truffle
+3. Ganache
+4. Metamask Extension
+
+## How To Run
+1. `npm install`
+2. Start ganache   
+3. `truffle compile`
+4. `truffle migrate --reset`
+4. `npm run dev`
---
+++ contracts/TodoList.sol
@@ -0,0 +1,33 @@
+pragma solidity ^0.6.12;
+
+contract TodoList {
+    uint public taskCount = 0;
+
+
+    struct Task {
+        uint id;
+        string content;
+        bool completed;
+    }
+
+
+    mapping(uint => Task) public tasks;
+
+    event TaskCreated(
+        uint id,
+        string content,
+        bool completed
+    );
+
+    function createTask(string memory _content) public {
+        taskCount ++;
+        tasks[taskCount] = Task(taskCount, _content, false);
+        emit TaskCreated(taskCount, _content, false);
+    }
+
+
+    constructor() public {
+        createTask("Tuntaskan materi blockchain fundamental");
+    }
+}
+
